9cca41d413d780455ac030abaa600fdcfc826ea0
cristophergarcia25 <cristopher.garcia@galileo.edu>
2025-07-15 20:30:58 -0600

    Landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,13 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: '*', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
+  { path: '*', component: LandingPageComponent },
   { path: '**', redirectTo: 'login' } // <-- Ruta comodín
 ];
