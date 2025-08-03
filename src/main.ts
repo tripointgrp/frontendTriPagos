d1a39fb009c4fca69b13a9fa21da20a8d6ab1beb
Abel <abel.alvarez@galileo.edu>
2025-08-03 10:16:16 -0600

    feat: :sparkles: Coneccion firebase

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 /// <reference types="@angular/localize" />
 
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAnalytics, getAnalytics } from '@angular/fire/analytics';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { environment } from './environments/environment';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideFirebaseApp(() => {
+      const app = initializeApp(environment.firebaseConfig);
+      console.log('✅ Firebase inicializado:', app.name);
+      return app;
+    }),
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+  ]
+}).catch(err => console.error(err));
+
